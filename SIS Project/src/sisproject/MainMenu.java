f92dac0b4e6381a687e395d7c0d603c6acc5608e
Claire Krawiec <c.m.krawiec12@gmail.com>
2016-11-15 14:50:57 -0700

    first method

---
+++ SIS Project/src/sisproject/MainMenu.java
@@ -0,0 +1,19 @@
+package sisproject;
+
+public class MainMenu
+{
+
+	
+	{
+		
+
+	}
+	public static void main()
+	{
+		System.out.println("What would you like to do?");
+		System.out.println("(1) Add or delete a student. ");
+		System.out.println("(2) Change students grade/ schedule.");
+		System.out.println("(3) Sort students.");
+	}
+
+}
