d7d2b40cf4101060c120e9b1adddf123f4d75023
nicklocascio <nicoloc4545@gmail.com>
2016-11-16 14:39:44 -0700

    My branch

--- SIS Project/src/Runner.java
+++ SIS Project/src/Runner.java
@@ -1,8 +1,9 @@
+import java.io.IOException;
 
 public class Runner
 	{
-	public static void main(String[] args)
+	public static void main(String[] args) throws IOException
 		{
-
+		makeChange.changeGrade();
 		}
 	}
--- SIS Project/src/makeChange.java
+++ SIS Project/src/makeChange.java
@@ -1,5 +1,13 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class makeChange
 	{
-	// this is quite tricky 
+	public static void changeGrade() throws IOException
+		{
+		Scanner file = new Scanner(new File("studentInformation.txt"));	
+		String student = file.nextLine();
+		System.out.println(student);
+		}
 	}
