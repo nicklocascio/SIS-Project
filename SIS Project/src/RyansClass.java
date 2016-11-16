c1381988d34e82760b0d6a245c56002019267d54
fergusonry <ryferg24@gmail.com>
2016-11-16 14:11:54 -0700

    i started the sorter

--- SIS Project/src/RyansClass.java
+++ SIS Project/src/RyansClass.java
@@ -1,6 +1,21 @@
+import java.util.Scanner;
 
 public class RyansClass
 	{
-		//syso(whatss sup)
+		static Scanner userInput = new Scanner(System.in);
+		public static void sorter()
+			{
+				System.out.println("what would you like to sort the students by?");
+				System.out.println("(1) sort by GPA");
+				System.out.println("(2) sort by last name");
+				System.out.println("(3) sort by class");
+				System.out.println("(4) I dont want to sort the students");
+				int answer = userInput.nextInt();
+				if (answer==1)
+					{
+						
+					}
+		
+			}
 
 	}
